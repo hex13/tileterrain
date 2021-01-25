5bc21f162f74117e762170f09d0b234e8db9ef8b
Łukasz Lityński <lukasz.litynski@gmail.com>
2021-01-25 18:32:14 +0100

    bufferCopyTiles;  and other things

--- src/createTerrain.js
+++ src/createTerrain.js
@@ -2,6 +2,10 @@ import * as THREE from 'three';
 
 let affected = Object.create(null);
 
+function fequal(a, b) {
+    return Math.abs(a - b) < 0.001;
+}
+
 function createFaceVertexUvs({ bottomLeftUv, bottomRightUv, topLeftUv, topRightUv, middleUv }) {
     const faceVertexUvs = [];
 
@@ -269,20 +273,112 @@ export function createTerrain({
         return edges;
     }
 
-    function copyTiles(targetGeom, tile, tile2, getMaterial = () => 0) {
-        targetGeom.vertices = [];
+    function inBounds(x, y) {
+        return x >= 0 && y >= 0 && x < columns && y < rows;
+    }
+    function bufferCopyTiles(targetGeom, tile, tile2, getMaterial = () => 0) {
+        const vertices = [];
+        const indices = [];
+        const uvs = [];
         targetGeom.faces = [];
+        if (!inBounds(tile.x, tile.y) || !inBounds(tile2.x, tile2.y)) {
+            console.error(`Tiles out ouf bounds: (${tile.x}, ${tile.y}), ({${tile2.x}, ${tile2.y})`);
+            return;
+        }
         let idx = 0;
+        const mapVertex = idx => {
+            const vertex = geometry.vertices[idx];
+            if (vertex.z < 0) {
+                return [vertex.x, vertex.y, 0];
+            }
+            return [vertex.x, vertex.y, vertex.z];
+        };
         for (let y = tile.y; y <= tile2.y; y++) {
             for (let x = tile.x; x <= tile2.x; x++) {
 
                 const geomTile = geometry.tiles[x][y];
+                if (!geomTile) {
+                    continue;
+                }
+
+                geomTile.faces.forEach(face => {
+                    vertices.push(
+                        ...mapVertex(face.a), ...mapVertex(face.b), ...mapVertex(face.c),
+                        // geometry.vertices[face.a],
+                        // geometry.vertices[face.b],
+                        // geometry.vertices[face.c],
+                    );
+                });
+
+                const mat = getMaterial(x,y);
+                indices.push(
+                    idx, idx + 1, idx + 2,
+                    idx + 3, idx + 4, idx + 5,
+                    idx + 6, idx + 7, idx + 8,
+                    idx + 9, idx + 10, idx + 11
+                );
+                idx += 4 * 3; // 4 faces, each face has 3 vertices
+
+                createFaceVertexUvs({
+                    bottomLeftUv: {x: 0, y: 0},
+                    bottomRightUv: {x: 1, y: 0},
+                    topLeftUv: {x: 0, y: 1},
+                    topRightUv: {x: 1, y: 1},
+                    middleUv: {x: 0.5, y: 0.5},
+                }).forEach(arr => {
+                    arr.forEach(vertexData => {
+                        uvs.push(vertexData.x, vertexData.y);
+                    })
+                    // uvs.push(arr);
+                });
+
+            }
+        }
+        targetGeom.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3))
+        targetGeom.setIndex(indices);
+
+        targetGeom.setAttribute('uv', new THREE.Float32BufferAttribute(uvs, 2));
+
+        targetGeom.facesNeedUpdate;
+        targetGeom.verticesNeedUpdate;
+        targetGeom.normalsNeedUpdate = true;
+        targetGeom.uvsNeedUpdate = true;
+
+        targetGeom.elementsNeedUpdate = true;
+        targetGeom.computeFaceNormals();
+        targetGeom.computeVertexNormals();
+
+    }
+
+    function copyTiles(targetGeom, tile, tile2, getMaterial = () => 0) {
+        targetGeom.vertices = [];
+        targetGeom.faces = [];
+        if (!inBounds(tile.x, tile.y) || !inBounds(tile2.x, tile2.y)) {
+            console.error(`Tiles out ouf bounds: (${tile.x}, ${tile.y}), ({${tile2.x}, ${tile2.y})`);
+            return;
+        }
+        let idx = 0;
+        const mapVertex = idx => {
+            const vertex = geometry.vertices[idx];
+            if (vertex.z < 0) {
+                return vertex.clone().set(vertex.x, vertex.y, 0);
+            }
+            return vertex;
+        };
+        for (let y = tile.y; y <= tile2.y; y++) {
+            for (let x = tile.x; x <= tile2.x; x++) {
+
+                const geomTile = geometry.tiles[x][y];
+                if (!geomTile) {
+                    continue;
+                }
 
                 geomTile.faces.forEach(face => {
                     targetGeom.vertices.push(...[
-                        geometry.vertices[face.a],
-                        geometry.vertices[face.b],
-                        geometry.vertices[face.c],
+                        mapVertex(face.a), mapVertex(face.b), mapVertex(face.c),
+                        // geometry.vertices[face.a],
+                        // geometry.vertices[face.b],
+                        // geometry.vertices[face.c],
                     ]);
                 });
 
@@ -323,7 +419,9 @@ export function createTerrain({
             geometry.normalsNeedUpdate = true;
             geometry.verticesNeedUpdate = true;
             geometry.computeFaceNormals();
-            geometry.computeVertexNormals();
+            // geometry.computeVertexNormals();
+            geometry.computeFlatVertexNormals();
+
         },
         clearAffectedList: () => {
             affected = Object.create(null);
@@ -348,8 +446,8 @@ export function createTerrain({
                 const lines = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({color: 0x000000}));
 
                 lines.rotation.x = - Math.PI / 2;
-                lines.position.y += 0.13;
-                lines.material.opacity = 0.3;
+                lines.position.y += 0.3;
+                lines.material.opacity = 0.14;
                 lines.material.transparent = true;
                 lines2 = lines;
                 return lines;
@@ -361,19 +459,62 @@ export function createTerrain({
                 clearTimeout(timeout);
                 timeout = setTimeout(() => {
                     let edges;
+                    lines1.geometry.dispose();
+                    edges = createGridGeometry();
+                    lines1.geometry = edges;
+
                     lines2.geometry.dispose();
                     edges = new THREE.EdgesGeometry(geometry);
                     lines2.geometry = edges;
 
-                    lines1.geometry.dispose();
-                    edges = createGridGeometry();
-                    lines1.geometry = edges;
                 }, 150);
 
             };
             return group;
         },
         copyTiles,
+        bufferCopyTiles,
         createGrid: createGridGeometry,
+        inBounds,
+        getElevationAt(tile) {
+            if (!inBounds(tile.x, tile.y)) {
+                console.error(`(${tile.x}, ${tile.y}) is out of map bounds`)
+                return {
+                    z: tile.z
+                }
+            }
+
+            try {
+            const tileData = tiles[tile.x][tile.y];
+            const middle = geometry.vertices[tileData.vertices.middleIdx].z;
+            const topLeft = geometry.vertices[tileData.vertices.topLeftIdx].z;
+            const topRight = geometry.vertices[tileData.vertices.topRightIdx].z;
+            const bottomLeft = geometry.vertices[tileData.vertices.bottomLeftIdx].z;
+            const bottomRight = geometry.vertices[tileData.vertices.bottomRightIdx].z;
+            const equal = topLeft 
+
+            const minElevation = Math.min(middle, topLeft, topRight, bottomLeft, bottomRight);
+            const maxElevation = Math.max(middle, topLeft, topRight, bottomLeft, bottomRight);
+
+            // const slope = Math.max(
+            //     Math.abs(middle - topLeft),
+            //     Math.abs(middle - topRight),
+            //     Math.abs(middle - bottomLeft),
+            //     Math.abs(middle - bottomRight),
+            // );
+            return {
+                z: middle,
+                topEqual: fequal(topLeft, topRight),
+                bottomEqual: fequal(bottomLeft, bottomRight),
+                leftEqual: fequal(topLeft, bottomLeft),
+                rightEqual: fequal(topRight, bottomRight),
+                slope: maxElevation - minElevation,
+                // slope: !(fequal(middle, topLeft) && fequal(middle, topRight) && fequal(middle, bottomLeft) && fequal(middle, bottomRight))
+            }
+            } catch (e) {
+                console.error(tile)
+            }
+            
+        }
     }
 }
